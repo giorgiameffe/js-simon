6636f58b023b37b078ec013502920b186cf62df4
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-06 19:03:02 +0100

    feat: add  display properties to form and add computer random number function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
 
-// Html elements
+// Elementi HTML
 
+// Countdown
 const countdownDiv = document.getElementById('countdown');
 console.log(countdownDiv);
 
+// Instructions
+const instructionsElement = document.getElementById('instructions');
+console.log(instructionsElement);
 
+// List
+const numberList = document.getElementById('number-list');
 
-// Countdown
+// Form
+const answerForm = document.getElementById('answers-form');
+
+// Inputs
+const inputElements = document.querySelectorAll('input');
+
+// Button
+const buttonElement = document.querySelector('button');
+
+// creare una funzione per gestire il countdown
+// inserire cambiamenti al termine del countdown
 
 let count = 10;
-let countDown2 = setInterval(countDown,1000);
+let countdownElement = setInterval(countDown,1_000);
 
 function countDown () {
 
@@ -17,10 +33,35 @@ function countDown () {
     count--;
 
     if (count === -1) {
-        clearInterval(countDown2);
-    }
-        
+        clearInterval(countdownElement);
+        instructionsElement.innerText = "Inserisci tutti i numeri che ricordi (l'ordine non è importante)";
+        answerForm.classList.remove('d-none');
+    }       
 }
+
+// funzione per generare numeri randomici per il computer
+
+function generateRandomNumbers (min, max) {
+
+    const randomArray = [];
+    const arrayLength = 5;
+    for (let i = 0; i < arrayLength; i++) {
+        const randomNum =  Math.ceil((Math.random() * max - min) + min);
+        randomArray.push(randomNum);
+    }
+    return randomArray;
+}
+
+const casualNumbers = generateRandomNumbers(1, 50);
+console.log(casualNumbers);
+
+
+
+
+
+
+
+
 
 
 
